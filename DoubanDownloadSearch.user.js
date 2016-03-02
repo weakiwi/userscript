8f9b5111f385bbe24a510d99fcbd5c69468ee48e
weakiwi <670932228@qq.com>
2016-03-02 08:58:56 +0800

    Update DoubanDownloadSearch.user.js
    增加对墙内友好的胖次网盘搜索（不稳定），以及书籍友好的readcolor，还有为己方便的海事图书馆搜索

--- DoubanDownloadSearch.user.js
+++ DoubanDownloadSearch.user.js
@@ -19,16 +19,20 @@ function run () {
 	var Movie_links = [
 		// { html: "百度盘", href: "http://www.baidu.com/s?wd=" + encodeURIComponent(keyword1 + " site:pan.baidu.com")},
 		{ html: "百度盘", href: "https://www.google.com/search?q=" + keyword1 + " site:pan.baidu.com"},
+		{ html: "胖次搜索", href: "http://www.panc.cc/s/" + keyword1},
 		{ html: "人人影视", href: "http://www.yayaxz.com/search/" + movieSimpleTitle },
 		{ html: "VeryCD", href: "http://www.verycd.com/search/folders/" + keyword2 },
 		{ html: "SimpleCD", href: "http://simplecd.me/search/entry/?query=" + keyword1 },
 		{ html: "Donkey4u", href: "http://donkey4u.com/search/" + movieTitle },
+		{ html: "dhtseek", href: "http://www.dhtseek.net/search/" + movieTitle + '.html' },
 		{ html: "Torrent Project", href: "http://torrentproject.com/?&btnG=Torrent+Search&num=20&start=0&s=" + keyword2 },
 		{ html: "Google MiniSD", href: "https://www.google.com/search?ie=UTF-8&q=" + movieTitle + "+MiniSD" }
 	];
 
 	var Book_links = [
 		{ html: "百度盘", href: "https://www.google.com/search?q=" + keyword1 + " site:pan.baidu.com"},
+		{ html : "大连海事图书馆", href: "http://202.118.84.130:1701/primo_library/libweb/action/search.do?ct=facet&fctN=facet_tlevel&fctV=available&rfnGrp=show_only&dscnt=0&frbg=&scp.scps=scope%3A(DLMH)%2Cprimo_central_multiple_fe&tab=default_tab&dstmp=1456801597218&srt=rank&ct=search&mode=Basic&&dum=true&indx=1&vl(freeText0)=" + keyword1 +"&fn=search&vid=dlmh"},
+                { html: "readcolor", href: "http://readcolor.com/books/search?utf8=%E2%9C%93&s=" + keyword2 },
 		{ html: "mLook", href: "http://www.mlook.mobi/search?q=" + keyword2 },
 		{ html: "VeryCD", href: "http://www.verycd.com/search/folders/" + keyword2 },
 		{ html: "SimpleCD", href: "http://simplecd.me/search/entry/?query=" + keyword1 },
