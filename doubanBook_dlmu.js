4a858d0e27cf20cdf7a6ad856b34656423aaad4d
weakiwi <670932228@qq.com>
2016-08-29 19:20:50 +0800

    Update doubanBook_dlmu.js
    把api扔到leancloud上了

--- doubanBook_dlmu.js
+++ doubanBook_dlmu.js
@@ -1,119 +1,26 @@
 // ==UserScript==
-// @name         豆瓣读书海事大学图书馆插件
-// @namespace    http://tampermonkey.net/
+// @name         dlmu_lib
+// @namespace    https://github.com/weakiwi
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  dlmu_lib
+// @run-at       document-idle
+// @author       weakiwi
 // @match        https://book.douban.com/subject/*
-// @grant        none
 // @grant        GM_xmlhttpRequest
+// @grant        GM_log
+// @require      http://cdn.bootcss.com/jquery/3.1.0/jquery.js
 // ==/UserScript==
-/* jshint -W097 */
-function run () {
-	var movieTitle = $('h1 span:eq(0)').text();
-	var title = $('html head title').text();
-	var keyword1 = title.replace( '(豆瓣)', '' ).trim();
-	var keyword2 = encodeURIComponent( keyword1 );
-	var movieSimpleTitle = movieTitle.replace(/第\S+季.*/, "");
-
-	var Movie_links = [
-		// { html: "百度盘", href: "http://www.google.com/s?wd=" + encodeURIComponent(keyword1 + " site:pan.baidu.com")},
-		{ html: "百度盘", href: "http://www.panc.cc/s/" + keyword1},
-		{ html: "SimpleCD", href: "http://simplecd.me/search/entry/?query=" + keyword1 },
-		{ html: "dhtseek", href: "http://www.dhtseek.net/search/" + movieTitle + '.html' },
-		{ html: "Torrent Project", href: "http://torrentproject.com/?&btnG=Torrent+Search&num=20&start=0&s=" + keyword2 },
-		{ html: "Google MiniSD", href: "https://www.baidu.com/search?ie=UTF-8&q=" + movieTitle + "+MiniSD" }
-	];
-
-	var Book_links = [
-		{ html: "百度盘", href: "http://www.panc.cc/s/" + keyword1},
-        { html : "大连海事图书馆", href: "http://202.118.84.130:1701/primo_library/libweb/action/search.do?ct=facet&fctN=facet_tlevel&fctV=available&rfnGrp=show_only&dscnt=0&frbg=&scp.scps=scope%3A(DLMH)%2Cprimo_central_multiple_fe&tab=default_tab&dstmp=1456801597218&srt=rank&ct=search&mode=Basic&&dum=true&indx=1&vl(freeText0)=" + keyword1 +"&fn=search&vid=dlmh"},
-		{ html: "readcolor", href: "http://readcolor.com/books/search?utf8=%E2%9C%93&s=" + keyword2 },
-		{ html: "Google", href: "https://www.google.com/search?ie=UTF-8&q=" + movieTitle },
-	];
-
-	var link = $("<div>").append(
-		$("<span>").attr("class", "pl").html("下载链接:")
-	);
-    isbn = getISBN();
-    function newztflh(book_name, book_location, book_ztflh, book_states) {
-        this.book_name = book_name;
-        this.book_location = book_location;
-        this.book_states = book_states;
-        this.book_ztflh = book_ztflh;
-    }
-    var ztflh = new newztflh('','','','');
-    GM_xmlhttpRequest({
-        method: "GET",
-        url: "http://115.28.1.228/xx1.php?search="+keyword1,
-        synchronous : false,
-        onload: function(response) {
-            var obj = JSON.parse(response.responseText);
-            if (obj.book_name == null) {
-                ztflh.book_name = "找不到该书籍";
-            }
-            else{
-                ztflh.book_name = obj.book_name;
-                ztflh.book_location = obj.book_location;
-                if (obj.book_states == null) {
-                    ztflh.booke_states = '';
-                }
-                else {
-                    ztflh.book_states = obj.book_states;
-                }
-                ztflh.book_ztflh = obj.book_ztflh;
-            }                
-            switch(location.host){
-                case "movie.douban.com":
-                    appendLinks(Movie_links, link)
-
-                    link.append('<br>')
-                        .append('<span class="pl">字幕链接:</span>')
-                        .append(
-                            $("<a>").attr({
-                                href: "http://shooter.cn/search/" + movieTitle,
-                                target: "_blank"
-                            }).html("Shooter")
-                        );
-
-                    break;
-                case "book.douban.com":
-                    appendLinks(Book_links, link)                    
-                    link.append('<br>')
-                        .append('<span class="pl">图书馆信息 :</span>'+'<br />'+ztflh.book_name+' '+'<br />'+ztflh.book_location+' '+'<br />'+ztflh.book_ztflh+' '+'<br />'+'<b>'+ztflh.book_states+'</b>'+'<br />')
-                    break;
-            }
-
-            $('#info').append(link);
+var book_title = $('[property="v:itemreviewed"]').text();
+GM_xmlhttpRequest({
+    method: "GET",
+    url: "https://teambition_addon.leanapp.cn/lib?search=" + book_title,
+    synchronous : false,
+    onload: function(response) {
+        var response_new =response.responseText;
+        var obj = JSON.parse(response_new);
+        GM_log(response_new);
+        if(obj.book_location) {
+            $('.gray_ad').eq(0).after('<div class="gray_ad" id="buyinfo"><div id="buyinfo-ebook"><h2 class=" ">图书馆收录信息</h2><ul class="bs noline more-after "><li><a target=_blank href="'+obj.book_url+'"><div class=basic-info><span>'+obj.book_location+'</span><span class=buylink-price><span>'+obj.book_states+'</span></span></div><div class=more-info><span class=buyinfo-promotion>'+obj.book_ztflh+'</span></div></a></ul></div></div>');
         }
-    });    
-
-
-	function appendLinks(items, appendTo){
-		items.forEach(function(item, i){
-			$("<a>")
-				.html(item.html)
-				.attr({
-					href: item.href,
-					target: "_blank"
-				})
-				.appendTo(appendTo);
-
-			if(i != items.length -1){
-				appendTo.append(" / ");
-			}
-		});
-	}
-    function getISBN(){
-        var rawBookInfo=document.getElementById("info").innerHTML;
-        var isbn = /ISBN:<\/span>(.*)<br>/.exec(rawBookInfo);
-        if (isbn !== null){
-            isbn = isbn[1].trim();
-        }
-        return isbn;
     }
-}
-
-run()
-
-// Your code here...
+});
